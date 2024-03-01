cbdf2a548d940743ab36f9e9653555c8f202cb60
IliaAzhel <aje-ilia2013@yandex.by>
2024-03-01 16:42:19 +0300

    clean

--- Staker/src/mappings/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
-
-
-export const stakerAddress = '0x32cff674763b06b983c0d55ef2e41b84d16855bb'
-export const vaultAddress = '0xdae024b1aa12df98d2fcf95b6b49a477f5d33dd0'
-export const oldVaultAddress = '0x796b1680fc26f1afc279b5b188b7e99d536d767f'
-export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-
-export let ZERO_BI = BigInt.fromI32(0)
