6dc43a7bb573528ba56ab38dd7a0be3f7c692144
IliaAzhel <aje-ilia2013@yandex.by>
2024-11-11 13:30:39 +0300

    fuse config

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xDfCC62B3eE40EE453Cb2788576B56e746FC79827'
+export const FACTORY_ADDRESS = '0xccEdb990abBf0606Cf47e7C6A26e419931c7dc1F'
 export const FEE_DENOMINATOR = BigDecimal.fromString('1000000') 
 
 export let ZERO_BI = BigInt.fromI32(0)
--- Algebra/src/utils/pricing.ts
+++ Algebra/src/utils/pricing.ts
@@ -4,14 +4,14 @@ import { Bundle, Pool, Token } from './../types/schema'
 import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { exponentToBigDecimal, safeDiv } from '../utils/index'
 
-const WMatic_ADDRESS = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'
-const USDC_WMatic_03_POOL = '0xb104f0535a35a69880dab51008756c31d47dbf0f'
+const WMatic_ADDRESS = '0x0be9e53fd7edac9f859882afdda116645287c629'
+const USDC_WMatic_03_POOL = '0xe0724a2d7adefbf7cae5aee22f8f68bf86c82d3f'
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
 export let WHITELIST_TOKENS: string[] = [
-  '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6', // WMATIC
-  '0xf2a0bc44debd394076c67962bb4869fd43c78018', // USDC
+  '0x0be9e53fd7edac9f859882afdda116645287c629', // WMATIC
+  '0x620fd5fa44be6af63715ef4e65ddfa0387ad13f5', // USDC
   '0x5aefba317baba46eaf98fd6f381d07673bca6467', // USDT 
   '0x49a390a3dfd2d01389f799965f3af5961f87d228'
 ]
@@ -21,7 +21,7 @@ let MINIMUM_Matic_LOCKED = BigDecimal.fromString('0')
 let Q192 = Math.pow(2, 192)
 
 let STABLE_COINS: string[] = [
-  '0xf2a0bc44debd394076c67962bb4869fd43c78018', // USDC
+  '0x620fd5fa44be6af63715ef4e65ddfa0387ad13f5', // USDC
   '0x5aefba317baba46eaf98fd6f381d07673bca6467' // SUDT
 ]
 
@@ -41,7 +41,7 @@ export function priceToTokenPrices(price: BigInt, token0: Token, token1: Token):
 export function getEthPriceInUSD(): BigDecimal {
   let usdcPool = Pool.load(USDC_WMatic_03_POOL) // dai is token0
   if (usdcPool !== null) {
-    return usdcPool.token0Price
+    return usdcPool.token1Price
   } else {
     return ZERO_BD
   }
